43495d3a6a03df3c10aa94801843886a08ab65cd
Sahand <sahand@pc-136-196.customer.sovereign.fortis.lan>
2019-01-17 20:00:56 +0000

    setup!

--- Main_Program/Main_Program.ino
+++ Main_Program/Main_Program.ino
@@ -9,6 +9,26 @@
 /*------------------------------------------------------------------------
   global variable declarations
   -------------------------------------------------------------------------*/
+#define LED 13
+
+// these might need to be tuned for different motor types
+#define NUM_SENSORS        6  // 
+#define REVERSE_SPEED     50 // 
+#define TURN_SPEED        100
+#define FORWARD_SPEED     100
+#define REVERSE_DURATION  200 // ms
+#define TURN_DURATION     150 // ms
+#define MAX_DISTANCE      30  // 
+#define TRIGGER_PIN        2  // 
+#define ECHO_PIN           6  // 
+#define STRING_TERMINATOR "!" // used as the last char to be sent over serial connection
+
+ZumoMotors motors;
+ZumoReflectanceSensorArray sensors;
+ZumoBuzzer buzzer;
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+
 
 
 void setup() {
